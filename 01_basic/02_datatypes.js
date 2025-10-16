5f5abbedc8f52fee4acb21589323925626f200e5
Rahul raj <rahulrajmaurya62058@gmail.com>
2025-10-16 15:25:39 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3) 
+
+console.log("rahul")
+
+
+let name = "rahul"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolea => treu/false
+// null => standalone value   > it is a object in js.
+// undefine => 
+// symbol => unique
+
+
+// object
+
+console.log (typeof undefined)
